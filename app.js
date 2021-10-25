93be60304686bb4dc1fa51f9607eba02ab121728
neprojo <53294140+neprojo@users.noreply.github.com>
2021-10-25 15:58:34 +0200

    edited JS for tabs

--- app.js
+++ app.js
@@ -14,13 +14,36 @@ form.addEventListener("submit", e => {
   }
 });
 
-let tabButton = document.querySelectorAll(".tabLink");
-let tabcontent = document.getElementsByClassName("tabcontent");
+//select tab buttons - test with console log
+const tablinks = document.querySelectorAll(".tablink");
+tablinks.forEach(tablink => console.log("Class list is " + tablink.classList));
 
-function changeTeamMember() {
-  // Hide all elements with class="tabcontent" by default */
-  for (i = 0; i < tabcontent.length; i++) {
-    tabcontent[i].style.display = "none";
-  }
-  //
+//select the contents of all the tabs
+const userBios = document.querySelectorAll(".tabcontent");
+
+//loop over tab buttons
+//add an event listener for when clicked
+// Hide all elements with class="tabcontent" by default
+for (let i = 0; i < tablinks.length; i++) {
+  tablinks[i].addEventListener("click", () => {
+    if (userBios[i].classList.contains("hidden")) {
+      userBios[i].classList.remove("hidden");
+    } else {
+      userBios[i].classList.add("hidden");
+    }
+  });
 }
+
+// let curIndex = 0;
+// function changeTeamMember() {
+//   curIndex++;
+//   console.log(curIndex);
+//
+// for (let j = 0; j < tabcontent.length; j++) {
+//   tabcontent[j].style.display = "block";
+// }
+// //Remove the background color of all tablinks/ buttons
+// for (let j = 0; j < tablinks.length; j++) {
+//   tablinks[j].style.backgroundColor = "";
+// }
+// }
